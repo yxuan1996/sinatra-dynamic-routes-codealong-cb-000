20f6c1a662099377ef51844ecb12fbf36584f5db
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-03 18:11:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get '/multiply/:num1/:num2' do
+  post '/multiply/:num1/:num2' do
     @number1 = params[:num1]
     @number2 = params[:num2]
     @number3 = @number1*@number2
